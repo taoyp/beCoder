8d39dfcdd566972a5750ad2c09d4363f954080e5
taoyp <taoyipingc@gmail.com>
2017-02-12 09:23:17 +0800

    dp add

---
+++ src/com/taoyp/pattern/PatternTest.java
@@ -0,0 +1,10 @@
+package com.taoyp.pattern;
+
+public class PatternTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This method is show for design pattern!");
+	}
+
+}
---
+++ src/com/taoyp/util/Print.java
@@ -0,0 +1,14 @@
+package com.taoyp.util;
+
+public class Print {
+
+	public static void print(){
+		System.out.println();
+	}
+	public static void println(Object object){
+		System.out.println(object);
+	}
+	public static void printnb(Object object){
+		System.out.print(object);
+	}
+}
